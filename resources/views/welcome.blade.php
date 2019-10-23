167d31ed9c193aa4986d4bf6be28484f7ac3ee69
samuelroam <samuelroam@gmail.com>
2019-10-23 08:46:39 +0200

    quitado fondo a los logos de la landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
                 <!--Media Martkt-->
                 <div id=mediamarkt class="tiendas">
                     <p><b>Media Markt</b></p>
-                    <img src="img/mediamarkt.jpg" alt="Media Markt">
+                    <img src="img/mediamarkt.png" alt="Media Markt">
                     <p class="descripcion trn" data-trn-key="mediamarkt_landing">Tienda para compra de aparatos electronicos y tecnologias</p>
                 </div>
                 <!--Zara-->
@@ -42,7 +42,7 @@
                 <!--Forum Sport-->
                 <div id=forumSport class=tiendas>
                     <p><b>Forum Sport</b></p>
-                    <img src="img/forum_sport.jpg" alt="Forum Sport">
+                    <img src="img/forum_sport.png" alt="Forum Sport">
                     <p class="descripcion trn" data-trn-key="forum_landing">Tienda de deportes y equipamiento deportivo</p>
                 </div>
                 <!--McDonalds-->
@@ -67,6 +67,6 @@
         </section>
 
         @include('footer')
-        
+
     </body>
 </html>
